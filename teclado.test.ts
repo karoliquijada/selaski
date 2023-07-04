9d000ce88b4de62caf902c78a78127e3dab20492
karoliquijada <kquijada9894@gmail.com>
2023-07-04 19:53:33 -0400

    Ajustes y documentacion

--- teclado.test.ts
+++ teclado.test.ts
@@ -1,15 +1,15 @@
-import {describe, expect, test} from '@jest/globals'
-import { calcularTiempo } from './teclado'
+import { expect, test} from '@jest/globals'
+import { countTime } from './teclado'
 
 const number = 1297;
 const keyboard1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const keyboard2 = [0, 1, 2, 9, 7, 5, 6, 3, 8, 4];
 
 test('Para la cifra 1297 y el caso 1 el resultado debe ser 11', () => {
-    expect(calcularTiempo(number, keyboard1)).toBe(11);
+    expect(countTime(number, keyboard1)).toBe(11);
 })
 
 
-test('Para la cifra 1297 y el caso 1 el resultado debe ser 4', () => {
-    expect(calcularTiempo(number, keyboard2)).toBe(4);
+test('Para la cifra 1297 y el caso 2 el resultado debe ser 4', () => {
+    expect(countTime(number, keyboard2)).toBe(4);
 })
--- teclado.ts
+++ teclado.ts
@@ -1,13 +1,26 @@
-const calcularTiempo = (num: number, keyboard: number[]): number => {
+/* Prueba rápida Saleski, Karoli Quijada.
+
+El approach fue utilizar fuerza bruta ya que al ser al azar había que probar todos los casos.
+Además tomé en cuenta que los datos fueran números y comenzara en 0, de ser no ser indicado
+se hubieran hecho las validaciones respectivas.
+
+Otra cosa es que asumí que los dígitos no se repetían (1222), en este caso
+el algoritmo sigue avanzando y se devuelve. Para este caso, haría la comprobación
+ de que el tiempo (time) aumente solo si el dígito no es igual anterior.
+
+*/
+const countTime = (num: number, keyboard: number[]): number => {
     let number = String(num)
       .split("")
       .map((num) => Number(num));
   
     let time = 0;
     let cont = 0;
+
+    const keyboardLength = keyboard.length;
   
     //Recorro el teclado para encontrar el primer dígito
-    for (let i = 0; i < keyboard.length; i++) {
+    for (let i = 0; i < keyboardLength; i++) {
       //Si lo encuentro aumento el contador para buscar el segundo dígito y así sucesivamente.
       //De esta manera no tengo que iterar también sobre el dígito.
       if (keyboard[i] === number[cont]) {
@@ -17,8 +30,8 @@ const calcularTiempo = (num: number, keyboard: number[]): number => {
       
       //Si llego a la ultima posicion del teclado y no he encontrado todos los digitos
       //comienzo a contar en reversa.
-      if (i === keyboard.length - 1 && cont < number.length) {
-        for (let j = keyboard.length - 1; j >= 0; j--) {
+      if (i === keyboardLength - 1 && cont < number.length) {
+        for (let j = keyboardLength - 1; j >= 0; j--) {
           if (keyboard[j] === number[cont]) {
             cont++;
           }
@@ -44,4 +57,4 @@ const calcularTiempo = (num: number, keyboard: number[]): number => {
     return time;
   };
   
-  export { calcularTiempo };
+  export { countTime };
